5775ae573c7b5b3cbfc7cda7e3533630b4dc6912
akshay8281 <akshaypitroda4@gmail.com>
2023-08-18 22:35:24 +0530

    If_ElseIf_Else-c

---
+++ If_ElseIF-Else.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+ {
+int a=100, b=20;
+
+if(a==100)
+{
+	if(b==200){
+	printf("The value of A is 100 and B is 200");	
+	}
+	else{
+		printf("Inside Else Statement");
+	}
+}
+
+else
+{
+	printf("Outside Else Statement");
+}
+
+  return 0;
+}
